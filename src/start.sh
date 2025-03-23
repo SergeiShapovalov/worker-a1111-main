ce269947cc9e9582a2145af1f957a3d0619ad93f
SergeiShapovalov <157950675+SergeiShapovalov@users.noreply.github.com>
2025-03-23 19:58:58 +0300

    Update start.sh

--- src/start.sh
+++ src/start.sh
@@ -15,8 +15,12 @@ if [ ! -f "/stable-diffusion-webui/embeddings/EasyNegative.safetensors" ]; then
     echo "Downloading EasyNegative embedding..."
     wget --content-disposition -P /stable-diffusion-webui/embeddings "https://huggingface.co/datasets/gsdf/EasyNegative/resolve/main/EasyNegative.safetensors?download=true"
 fi
+# Запускаем cache.py для предварительной загрузки модели в GPU
+echo "Preloading model with cache.py..."
+cd /stable-diffusion-webui && python cache.py --ckpt /stable-diffusion-webui/models/Stable-diffusion/flux_checkpoint.safetensors
 
 echo "Starting Forge WebUI API"
+
 python /stable-diffusion-webui/webui.py --skip-python-version-check --skip-torch-cuda-test --skip-install --ckpt flux_checkpoint.safetensors --lowram --disable-safe-unpickle --port 3000 --api --nowebui --skip-version-check --no-hashing --no-download-sd-model --forge-preset flux &
 
 echo "Starting RunPod Handler"
